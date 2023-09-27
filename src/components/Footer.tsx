0d124d154fe47d58511c4e3abaf2c0313336c906
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-09-27 10:04:11 -0300

    create footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="bg-zinc-800 mt-10 flex py-20">
+      <div className="">
+        <div>Logo</div>
+      </div>
+
+      <div className="flex flex-1 gap-4 justify-center">
+        <Link href={"/"}>Acerca de Volve</Link> |
+        <Link href={"/"}>Steamworks</Link> |<Link href={"/"}>Empleo</Link> |
+        <Link href={"/"}>Distribucion de Steam</Link> |
+        <Link href={"/"}>Tarjetas de regalo</Link> |
+        <Link href={"/"}>Steam</Link> |<Link href={"/"}>@steam</Link>
+      </div>
+    </footer>
+  );
+}
