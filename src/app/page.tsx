9097127f8985a3d862af8773f286ddde5cd2a86e
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-09-27 08:59:53 -0300

    create search input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import axios from "axios";
+import Image from "next/image";
 import { useEffect } from "react";
+import residentEvil from "../../public/images/resident-evil-4.png";
 
 export default function Home() {
   const endpoint =
@@ -13,5 +15,33 @@ export default function Home() {
   //   console.log(result);
   // }, []);
 
-  return <main className="">Dev challenge</main>;
+  return (
+    <main className="h-full">
+      <Image
+        src={residentEvil}
+        alt="Home picture"
+        className="min-w-full max-h-[400px] object-fill"
+      />
+
+      <div className="flex justify-center my-8 rounded border border-pink-600 align-middle">
+        <input
+          type="text"
+          className="bg-transparent placeholder:text-pink-600"
+          placeholder="Search"
+        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-6 h-6 text-pink-600"
+        >
+          <path
+            fillRule="evenodd"
+            d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </div>
+    </main>
+  );
 }
