6eb3d391eaf2915b7af7f90816665f8a241f165c
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-10-07 12:01:37 -0300

    add rounded price to the cart

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,6 +9,8 @@ import logo from "../../public/images/logo.png";
 export default function Navbar() {
   const { totalPrice } = useContext(TotalPriceContext);
 
+  const roundedPrice = Math.round((totalPrice + Number.EPSILON) * 100) / 100;
+
   return (
     <nav className="max-w-full flex justify-between gap-20 py-10 items-center mx-80">
       <Image
@@ -34,7 +36,7 @@ export default function Navbar() {
           >
             <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
           </svg>
-          Cart <span className="font-semibold">${totalPrice}</span>
+          Cart <span className="font-semibold">${roundedPrice}</span>
         </Link>
       </div>
     </nav>
